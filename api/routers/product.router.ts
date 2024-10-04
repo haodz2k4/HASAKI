95b2ff4f7c611cfe4e5d845d295882f550793ebd
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-04 14:01:57 +0700

    Write Swagger for test api

--- api/routers/product.router.ts
+++ api/routers/product.router.ts
@@ -1,6 +1,16 @@
 import { Router } from "express";
 const router: Router = Router();
 import * as handler from "../handler/product.handler"
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     summary: Lấy danh sách sản phẩm
+ *     description: Trả về danh sách tất cả các sản phẩm.
+ *     responses:
+ *       200:
+ *         description: Thành công
+ */
 router.get("/",handler.getProducts)
 router
     .route("/:id")
