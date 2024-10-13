7f7cad65db6d5d720d4807df7f3b1a51a8fef172
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-13 11:23:36 +0700

    Hasaki

--- controllers/admin/user.controller.ts
+++ controllers/admin/user.controller.ts
@@ -2,12 +2,22 @@ import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import userModel from "../../models/user.model";
 
+import paginationHelper from "../../helpers/pagination.helper";
+
 //[GET] "/admin/users"
 export const users = catchAsync(async (req: Request, res: Response) => {
     
-    
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 20;
+    const [users, count] = await Promise.all([
+        await userModel.find({deleted: false}),
+        await userModel.countDocuments({deleted: false})
+    ])
+    const pagination = paginationHelper(page, limit, count);
     res.render("admin/pages/users/user.pug",{
         pageTitle: "Quản lý người dùng",
-        activePages: "users"
+        activePages: "users",
+        users,
+        pagination
     })
 })
--- middleware/clients/user.middleware.ts
+++ middleware/clients/user.middleware.ts
@@ -8,12 +8,16 @@ export default catchAsync(async (req: Request, res: Response, next: NextFunction
     
     const token = req.cookies.accessToken;
     if(token){
-        const payload = verify(token,config.jwt_user.jwt_access_secret as string);
-        const {_id} = payload as JwtPayload;
-        const user = await userModel.findOne({_id, deleted: false});
-        if(user && user.status === 'active'){
-            res.locals.user = user 
-        }
+        try {
+            const payload = verify(token,config.jwt_user.jwt_access_secret as string);
+            const {_id} = payload as JwtPayload;
+            const user = await userModel.findOne({_id, deleted: false});
+            if(user && user.status === 'active'){
+                res.locals.user = user 
+            }
+            //If jwt expire or jwt invalid => next 
+        } catch {}
+        
     }
     next()
 })
