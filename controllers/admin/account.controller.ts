63c8e73c25d16e59496f43fe2fce36b71cfa585f
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-15 09:04:16 +0700

    Manage account and design page create

--- controllers/admin/account.controller.ts
+++ controllers/admin/account.controller.ts
@@ -1,14 +1,36 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import accountModel from "../../models/account.model";
+import roleModel from "../../models/role.model";
+import paginationHelper from "../../helpers/pagination.helper";
 
 //[GET] "/admin/accounts"
 export const accounts = catchAsync(async (req: Request, res: Response) => {
-
-    const accounts = await accountModel.find({deleted: false})
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 5;
+    const skip = (page - 1) * limit;
+    const [accounts, total] = await Promise.all([
+        accountModel
+            .find({deleted: false})
+            .limit(limit)
+            .skip(skip),
+        accountModel.countDocuments({deleted: false})
+    ])
+    const pagination = paginationHelper(page, limit, total);
     res.render("admin/pages/accounts/account.pug",{
         pageTitle: "Quản lý tài khoản",
         activePages: "accounts",
-        accounts
+        accounts,
+        pagination
+    })
+})
+
+//[GET] "/admin/accounts/create"
+export const create = catchAsync(async (req: Request, res: Response) => {
+    const roles = await roleModel.find({deleted: false})
+    res.render("admin/pages/accounts/create.pug",{
+        pageTitle: "Thêm tài khoản",
+        activePages: "accounts",
+        roles
     })
 })
--- routers/admin/account.router.ts
+++ routers/admin/account.router.ts
@@ -3,4 +3,7 @@ const router: Router = Router()
 import * as controller from "../../controllers/admin/account.controller";
 router.get("/", controller.accounts)
 
+router
+    .route("/create")
+    .get(controller.create)
 export default router;
