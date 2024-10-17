b4baf2af3239e83e13bb9a27c4ca08175cbc3fe5
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-17 10:51:14 +0700

    Push data and aggrate to view

--- controllers/admin/inventory.controller.ts
+++ controllers/admin/inventory.controller.ts
@@ -1,13 +1,27 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import inventoryModel from "../../models/inventory.model";
+import paginationHelper from "../../helpers/pagination.helper";
 
 //[GET] "/admin/inventories"
 export const inventory = catchAsync(async (req: Request, res: Response) => {
-    const inventories = await inventoryModel.find({deleted: false})
+
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 5;
+    const skip = (page - 1) * limit;
+    const [inventories, total] = await Promise.all([
+        inventoryModel
+        .find({deleted: false})
+        .skip(skip)
+        .populate('productId','title')
+        .populate('supplierId','name'),
+        inventoryModel.countDocuments({deleted: false})
+    ])
+    const pagination = paginationHelper(page, limit, total)
     res.render("admin/pages/inventories/inventory.pug",{
         inventories,
         activePages: 'inventories',
-        pageTitle: 'Quản lý kho hàng'
+        pageTitle: 'Quản lý kho hàng',
+        pagination
     })
 })
