ef459161099990858339e1df21222b18002e5fce
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-17 08:18:43 +0700

    Page and tool category

--- controllers/admin/category.controller.ts
+++ controllers/admin/category.controller.ts
@@ -1,14 +1,42 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import categoryModel from "../../models/category.model";
+import paginationHelper from "../../helpers/pagination.helper";
+import filterHelper from "../../helpers/filter.helper";
+import pick from "../../utils/pick";
 
 //[GET] "/admin/category"
 export const category = catchAsync(async (req: Request, res: Response) => {
-    const categories = await categoryModel.find({deleted: false});
+    //Pagination 
+    const page = parseInt(req.query.page as string) || 1;
+    const limit = parseInt(req.query.limit as string) || 5;
+    const skip = (page - 1) * limit;
+    //Filter 
+    const filter: Record<string, unknown> = {deleted: false};
+    //Filters 
+    const filters = filterHelper([]);
+    const status = req.query.status as string;
+    if(status){
+        filter.status = status;
+        const index = filters.findIndex(item => item.name === status);
+        filters[index].selected = true 
+    }
+    //Query 
+    const [categories,total] = await Promise.all([
+        categoryModel
+            .find(filter)
+            .skip(skip)
+            .limit(limit),
+        categoryModel.countDocuments(filter)
+    ])
+    const pagination = paginationHelper(page, limit, total)
     res.render("admin/pages/categories/category.pug",{
         pageTitle: "Quản lý danh mục",
         categories,
         activePages: 'categories',
+        pagination,
+        filters
     })
 })
+
 
