13fbe21ff8acb07cf9c06ec4c3615fbce14210dc
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-18 09:22:14 +0700

    Require Permission

--- api/middlewares/auth.middleware.ts
+++ api/middlewares/auth.middleware.ts
@@ -7,7 +7,7 @@ import { getAccountById } from "../services/account.service";
 
 export const requireAuth = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     if(!req.headers.authorization){
-        throw new ApiError(403,"Token is must provided")
+        throw new ApiError(401,"Token is must provided")
     }
     const token = req.headers.authorization.split(" ")[1];
     const payload = verify(token,config.jwt.admin.jwt_access_secret as string);
@@ -17,6 +17,22 @@ export const requireAuth = catchAsync(async (req: Request, res: Response, next: 
     if(!account){
         throw new ApiError(401,"Account is not found");
     }
+    res.locals.account = account
     next()
 
 })
+
+export const requirePermission = (permission: string) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const account = res.locals.account;
+        console.log(account)
+        console.log(permission)
+        //if role is admin check the permission and if role is user not check 
+        if(account){
+            if(!account.roleId.permissions.includes(permission)){
+                throw new ApiError(403,"You do not have enough authority")
+            }
+        }
+        next()
+    }
+}
--- api/routers/product.router.ts
+++ api/routers/product.router.ts
@@ -6,20 +6,20 @@ import { storage } from "../../storage/cloud";
 import multer from "multer";
 const upload = multer({ storage });
 
-import { requireAuth } from "../middlewares/auth.middleware";
+import { requireAuth, requirePermission } from "../middlewares/auth.middleware";
 router  
     .route("/")
-    .get(controller.getProducts)
-    .post(requireAuth,controller.createProduct)
+    .get(requirePermission('product_view'),controller.getProducts)
+    .post(requireAuth,requirePermission('product_create'),controller.createProduct)
 
 router
     .route("/:id")
-    .get(controller.getProductById)
-    .patch(requireAuth,controller.updateProduct)
-    .delete(requireAuth,controller.deleteProduct)
-router.get("/slug/:slug",controller.getProductBySlug)
+    .get(requirePermission('product_view'),controller.getProductById)
+    .patch(requireAuth,requirePermission('product_update'),controller.updateProduct)
+    .delete(requireAuth,requirePermission('product_delete'),controller.deleteProduct)
+router.get("/slug/:slug",requireAuth,requirePermission('product_view'),controller.getProductBySlug)
 router
     .route("/:id/upload")
-    .post(requireAuth,upload.single('avatar'),controller.uploadFile)
+    .post(requireAuth,requirePermission('product_create'),upload.single('avatar'),controller.uploadFile)
 
 export default router
