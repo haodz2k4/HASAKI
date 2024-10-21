ba3b6b634b42ea00b9aba975b20767d3b1563b65
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-21 09:34:38 +0700

    Update pagination and range price

--- public/js/script.js
+++ public/js/script.js
@@ -147,34 +147,32 @@ function changeOverlayImage(thumbnail) {
   overlayImage.src = thumbnail.src;
 }
 
-// Thiết lập thời gian kết thúc (ví dụ: sau 12 giờ từ hiện tại)
-const countdownDate = new Date().getTime() + 12 * 60 * 60 * 1000; // 12 giờ từ bây giờ
+const countdownDate = new Date().getTime() + 12 * 60 * 60 * 1000; 
 
 const timer = setInterval(function () {
   const now = new Date().getTime();
   const distance = countdownDate - now;
 
-  // Tính toán giờ, phút, giây còn lại
   const hours = Math.floor(
     (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
   const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Hiển thị đồng hồ đếm ngược
-  document.getElementById('countdown-timer').innerHTML =
+  const countDown = document.getElementById('countdown-timer')
+  if(countDown){
+    countDown.innerHTML =
     '0 : ' +
     (hours < 10 ? '0' + hours : hours) +
     ' : ' +
     (minutes < 10 ? '0' + minutes : minutes) +
     ' : ' +
     (seconds < 10 ? '0' + seconds : seconds);
-
-  // Nếu hết thời gian, dừng đồng hồ và hiển thị thông báo
-  if (distance < 0) {
-    clearInterval(timer);
-    document.getElementById('countdown-timer').innerHTML = 'Hết giờ';
+    if (distance < 0) {
+      clearInterval(timer);
+      document.getElementById('countdown-timer').innerHTML = 'Hết giờ';
+    }
   }
+  
 }, 1000);
 
 function changeColor(color) {
@@ -188,6 +186,8 @@ if(btnPagination.length > 0){
       const page = item.getAttribute("btn-pagination");
       if(page){
         url.searchParams.set("page", page)
+      }else{
+        url.searchParams.delete("page")
       }
       window.location.href = url.href
     })
