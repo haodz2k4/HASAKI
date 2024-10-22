94d450bc3f8253b68e12d6c99f42bda5347eb282
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-22 14:06:09 +0700

    Cart middleware

---
+++ middleware/clients/cart.middleware.ts
@@ -0,0 +1,15 @@
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import cartModel from "../../models/cart.model";
+
+export default catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const user = res.locals.user;
+    if(!user){
+        let cart = await cartModel.findOne({userId: user.id});
+        if(!cart){
+           cart = await cartModel.create({userId: user.id});
+        }
+        res.locals.cart = cart
+    } 
+    next()
+})
